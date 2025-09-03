7ffc33b811c75c8287517efb60593a15de5da88b
fercha <fer.charubi93@gmail.com>
2025-09-02 23:06:48 -0300

    ejercicio 7

--- Practica.js/Ejercicio-07.js
+++ Practica.js/Ejercicio-07.js
@@ -5,11 +5,9 @@ const num3 = parseInt(prompt("ingrese el tercer numero"));
 if (num1 > num2) {
   document.writeln(num1);
 } else {
-  if (num2 > num1) {
-    document.writeln(num2);
+  if (num3 > num1 && num3 > num2) {
+    document.writeln(num3);
   } else {
-    if (num3 > num1 && num2) {
-      document.writeln(num3);
-    }
+    document.writeln(num2);
   }
 }
