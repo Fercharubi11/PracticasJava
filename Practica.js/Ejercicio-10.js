27784d3f8e2a4977d386e71164c786f32bd3179f
fercha <fer.charubi93@gmail.com>
2025-09-03 01:11:16 -0300

    ejercicio 10

---
+++ Practica.js/Ejercicio-10.js
@@ -0,0 +1,14 @@
+/* Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 
+(sólo hay que comprobar si lo es por uno de los cuatro)*/
+const numero = parseInt(prompt("ingrese un numero"));
+if (numero % 2 == 0) {
+  document.writeln("el " + numero + " es divisible por 2");
+} else {
+  if (numero % 3 == 0) {
+    document.writeln("el " + numero + " es divisible por 3");
+  } else if (numero % 5 == 0) {
+    document.writeln("el " + numero + " es divisible por 5");
+  } else {
+    document.writeln("el " + numero + " es divisible por 7");
+  }
+}
